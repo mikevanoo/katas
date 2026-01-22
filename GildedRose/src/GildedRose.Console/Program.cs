64d638e22d98e535713999e3be245cd5f92fa480
Mike van Oorschot <michael.oorschot@codurance.com>
2026-01-22 13:41:25 +0000

    Refactored `Program.cs` to simplify item list initialization and streamline the `UpdateQuality` logic.

--- GildedRose/src/GildedRose.Console/Program.cs
+++ GildedRose/src/GildedRose.Console/Program.cs
@@ -5,64 +5,54 @@ namespace GildedRose.Console
 {
     class Program
     {
-        IList<Item> Items; // can't change this property
-        
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
-
-            var app = new Program()
+            
+            IList<Item> items = new List<Item>
             {
-                Items = new List<Item>
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item
                 {
-                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                    new Item
-                    {
-                        Name = "Backstage passes to a TAFKAL80ETC concert",
-                        SellIn = 15,
-                        Quality = 20
-                    },
-                    new Item
-                    {
-                        Name = "Backstage passes to a TAFKAL80ETC concert",
-                        SellIn = 10,
-                        Quality = 49
-                    },
-                    new Item
-                    {
-                        Name = "Backstage passes to a TAFKAL80ETC concert",
-                        SellIn = 5,
-                        Quality = 49
-                    },
-                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                }
-
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 49
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
+            
+            var app = new GildedRose(new ItemUpdateStrategyFactory());
 
             const int days = 30;
             for (var i = 0; i <= days; i++)
             {
                 System.Console.WriteLine("-------- day " + i + " --------");
                 System.Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < app.Items.Count; j++)
+                foreach (var item in items)
                 {
-                    System.Console.WriteLine(app.Items[j].Name + ", " + app.Items[j].SellIn + ", " + app.Items[j].Quality);
+                    System.Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
                 }
                 System.Console.WriteLine("");
-                app.UpdateQuality();
+                app.UpdateQuality(items);
             }
 
             System.Console.ReadKey();
-        }
-
-        private void UpdateQuality()
-        {
-            var gildedRose = new GildedRose(new ItemUpdateStrategyFactory());
-            gildedRose.UpdateQuality(Items);
         }
     }
 }
